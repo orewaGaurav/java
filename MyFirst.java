1ccaec4a5e3b6f5723d984dc99db6850969c2a56
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-08-22 21:57:10 +0530

    java

--- MyFirst.java
+++
@@ -1,9 +0,0 @@
-package myfirst;
-
-public class MyFirst {
-
-    public static void main(String[] args) {
-       System.out.println("Hello World");
-    }
-    
-}
---
+++ first.java
@@ -0,0 +1,16 @@
+// public class MyFirst{
+//     public static void main(String[] args){
+//         System.out.print("Gaurav\n");
+//     }
+// }
+
+public class first{
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.print("* * *\n");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        
+    }
+}
