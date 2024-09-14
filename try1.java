56588578f9a95c6c5277b5825a7cd2ebf58b18e5
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-15 00:14:35 +0530

    java basics

---
+++ try1.java
@@ -0,0 +1,7 @@
+public class try1{
+    public static void main(String args[]){
+        System.out.println("Hlw\nGaurav");
+        System.out.println("JDK-java development kit\nThis is used to run java code\nIt contains JRE(java runtime enviornment) and also many development tools");
+        System.out.println("JRE also contains JVM-java virtual machine");
+    }
+}
