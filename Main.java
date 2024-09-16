5bdc51b3c097a7927a039a02e64604b20ad7e2e6
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-16 18:44:11 +0530

    JAVA BASICS

--- Main.java
+++ Main.java
@@ -31,8 +31,62 @@ public class Main{
         System.out.println(txt.indexOf("locate")); // Outputs 7
         String txt1 = "Please locate \"where\" locate \'occurs!\'";
         System.out.println(txt1);
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-4.7)); 
+        System.out.println((int)(Math.random()*101));
+        // ternory operator
+        // variable = (condition) ? expressionTrue :  expressionFalse;
+        int time = 20;
+        int num5 =40; 
+        String result = (time < 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
+        // ternory operator for more than one condition;
+        // condition1 ?(condition2 ? Expression1 : Expression2) : Expression3; 
+        String res = (time<18)?(num5>time?"yes":"No"):"no";
+        System.out.println(res);
+        // switch
+        int day = 2;
+        switch(day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thursday");
+            break;
+        }
+        int i =0;
+        while (i<=10){
+            System.out.println(i);
+            i++;
+        }
+        // The do/while loop is a variant of the while loop. This loop will execute the code block once, 
+        // before checking if the condition is true,then it will repeat the loop as long as the condition is true.
+        i=0;
+        do{
+            System.out.println(i);
+            i++;
+        }
+        while(i>3);
+        // condition is false but loop will execute for 1st time
+        for(i=0;i<5;i++)
+        System.out.print(" "+i);
+
+        // for each-loop
+        System.out.println("\n");
+        String[] cars = {"BMW","VOLVO","LAMBO","MERCEDES"};
+        for(String j:cars){
+            System.out.println(j);
+        }
 
     
+
+
 
 
     }
