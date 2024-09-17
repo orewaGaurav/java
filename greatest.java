1441445f012b157bea7a2c24ee1c1508b3a81d89
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-17 14:24:49 +0530

    greatest of three num

---
+++ greatest.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class greatest {
+    public static void main(String args[]){
+        int a,b,c,g;
+        Scanner i = new Scanner(System.in);
+        System.out.println("Enter three num: ");
+        a = i.nextInt();
+        b = i.nextInt();
+        c = i.nextInt();
+        if(a>b&&a>c)
+        g=a;
+        else if(b>a&&b>c)
+        g=b;
+        else;
+        g = c;
+        System.out.println("The greatest is : "+g);
+    }
+}
