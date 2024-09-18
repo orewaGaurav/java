613b24ed1258516762aa5da717d404fcc3f262be
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-18 11:34:34 +0530

    balanceCheck

---
+++ balanceCheck.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class balanceCheck {
+    public static void balance(double x){
+        if(x>1000){
+        System.out.println("Balance is sufficient");
+        }
+        else
+        System.out.println("Balance is low");
+
+    }
+    public static void main(String args[]){
+        double x;
+        System.out.println("Enter the balance: ");
+        Scanner i = new Scanner(System.in);
+        x = i.nextDouble();
+        balance(x);
+    }
+}
