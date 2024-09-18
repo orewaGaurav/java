539ad38b1ce4ba5834b43086a15f68402e7a6ee3
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-19 00:13:04 +0530

    count no. of digit

---
+++ countDigit.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class countDigit {
+    public static void main(String args[]){
+        System.out.print("Enter the num: ");
+        Scanner g = new Scanner(System.in);
+        int num = g.nextInt();
+        int d,digit=0;
+        while(num>0){
+            d=num%10;
+            num=num/10;
+            digit++;
+        }
+        System.out.println("Number of digit: "+digit);
+    }
+}
