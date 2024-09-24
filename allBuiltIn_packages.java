b551e662283ea5096a2c91170fb323ecccdcde76
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-24 11:32:23 +0530

    builtin package + creating package

---
+++ allBuiltIn_packages.java
@@ -0,0 +1,26 @@
+import java.util.*;//packages for dsa and input
+import java.io.*;//for input output operations
+import java.lang.*;//for contains classes for primitive data types
+import java.awt.*;//for GUI
+import java.applet.*;//for creating applets
+import java.net.*;//for networking operation
+
+@SuppressWarnings("unused")
+public class allBuiltIn_packages {
+    public static void main(String args[]){
+        //to create user defined packages
+
+        /*package mypackages;
+            public class create_packages {
+            public static void main(String args[]){
+            System.out.println("Welcome to OOT using java class")
+    }  
+}
+ */
+        System.out.println("step1-create a folder give the same name that of package name that u want to create");
+        System.out.println("step2-now create a java file containing code to create package and name it by classname.java");
+        System.out.println("step3-now compile the code inside the packageName folder by javac filename.java");
+        System.out.println("step4-now come out of package folder and write java packageName.className");
+    }
+    
+}
