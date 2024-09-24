12253b185cf447bb516745493ec7923985f9ce53
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-24 14:14:28 +0530

    Exception handling

---
+++ execHandling.java
@@ -0,0 +1,21 @@
+public class execHandling{
+    public static void main(String[] args) {
+        try{
+            int arr[]= {2,3,4,5,6,7,8,9};
+            System.out.println(arr[10]);
+        
+        }
+        catch(Exception e){
+        System.out.println("Array out of index");
+        }
+        System.out.println("REST");
+        try{
+        int z = 5;
+        System.out.println(z/0);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Its a blunder");
+        }
+    
+    }
+}
