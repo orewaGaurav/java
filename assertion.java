d48de9363391df58b9ab4183db67474672295e66
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-25 10:01:41 +0530

    Assertion -ea

---
+++ assertion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class assertion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+        assert (age >=18 && age<=60): "Not Valid candidate";
+        System.out.println("age is "+age);
+    }
+}
+// to run it we hve to first enable assertion
+//first compile it by javac fileName.java
+// then java -ea fileName 
