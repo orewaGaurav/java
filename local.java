bfd82a357a6802ca2e150f77cbbdc7f6d26fde35
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-25 10:21:05 +0530

    localization

---
+++ local.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class local {
+    public static void main(String[] args) {
+        Locale l = Locale.getDefault();
+        System.out.println(l.getDefault());
+        System.out.println(l.getDisplayCountry(l));
+        System.out.println(l.getDisplayLanguage());
+        System.out.println(l.getCountry());
+        
+        
+        
+        
+    }
+    
+}
