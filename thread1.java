1e0c35a704ea00e5b80eab843b33af9dcb523849
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-26 11:25:21 +0530

    Thread

---
+++ thread1.java
@@ -0,0 +1,24 @@
+// import java.util.*;
+class thread1 extends Thread {
+    public void run(){
+        for(int i=1;i<=5;i++){
+            try{
+                Thread.sleep(2000);
+                System.out.println(i);
+            }
+            catch(Exception e){
+                System.out.println(e);
+                System.out.println(i);
+            }
+        }
+    }
+    public static void main(String args[]){
+        thread1 t1 = new thread1();
+        thread1 t2 = new thread1();
+        System.out.println("\nResult will be display after 5 sec: please WAIT");
+        t1.start();
+        t2.start();
+        System.out.println("Gaurav");
+        // t1.run();
+    }
+}
