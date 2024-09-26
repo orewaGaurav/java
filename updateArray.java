94b6eb3f6f8a5f3dbf446bb6986668c608172706
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-26 09:59:32 +0530

    update array

--- updateArray.java
+++ updateArray.java
@@ -1,36 +1,34 @@
 import java.util.*;
-public class updateArray{
-    @SuppressWarnings("resource")
+public class updateArray {
     public static void main(String args[]){
-        Scanner g = new Scanner(System.in);
+        Scanner g =new Scanner(System.in);
         System.out.println("Enter the no. of elements: ");
-        int n = g.nextInt();
-        int arr[]=new int[n];
-        int newArr[]=new int[n+1];
-        for(int i=0;i<n;i++){
-            arr[i]=g.nextInt();
+        int n =g.nextInt();
+        int arr[] = new int[n];
+        int k = 0;
+        while(k<n){
+            arr[k]=g.nextInt();
+            k++;
         }
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
+        for(int i =0;i<n;i++){
+            if(i<n-1)
+            System.out.print(arr[i]+",");
+            else
+            System.out.print(arr[i]);
         }
-        System.out.println(" ");
-        System.out.println("Enter the element to insert: ");
-        int value = g.nextInt();
+        System.out.print("\n");
+        System.out.println("Enter the element to update: ");
+        int n1 = g.nextInt();
         System.out.println("enter the index: ");
-        int index =g.nextInt();
-        for(int i=0;i<n;i++){
-            if(arr[i]==index){
-                for(int j =i;j<n-1;j++){
-                    arr[j]=arr[j+1];
-                }
-                n--;
-            }
+        int index= g.nextInt();
+        arr[index]=n1;
+        for(int i =0;i<n;i++){
+            if(i<n-1)
+            System.out.print(arr[i]+",");
+            else
+            System.out.print(arr[i]);
         }
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]+" ");
-        }
-    
-
+        g.close();
 
     }
 }
