8a730527404425ea572d5692c97fa70c59bd1ae0
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-26 11:31:41 +0530

    Thread

--- thread1.java
+++ thread1.java
@@ -4,12 +4,13 @@ class thread1 extends Thread {
         for(int i=1;i<=5;i++){
             try{
                 Thread.sleep(2000);
-                System.out.println(i);
+                
             }
             catch(Exception e){
                 System.out.println(e);
-                System.out.println(i);
+                
             }
+            System.out.println(i);
         }
     }
     public static void main(String args[]){
