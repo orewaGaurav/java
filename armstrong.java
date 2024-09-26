1965cffc34eefb5a45e4cd1bbd8dd0e5e92920f9
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-26 14:28:44 +0530

    Armstrong

--- armstrong.java
+++ armstrong.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class armstrong {
+    @SuppressWarnings({"resource","unused"})
     public static void main(String args[]){
         System.out.print("Enter the number: ");
         Scanner g = new Scanner(System.in);
