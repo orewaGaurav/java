f815d7913f1b2204044d37a719646711c2e59fff
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-28 10:43:47 +0530

    perfect number check

--- perfectNum.java
+++ perfectNum.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class perfectNum {
+    @SuppressWarnings("resource")
     public static void main(String args[]){
         System.out.print("Enter the num: ");
         Scanner g = new Scanner(System.in);
