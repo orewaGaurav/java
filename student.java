817fd4d4e7b756294329c29b053ca5992d546ab6
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-09-29 11:41:59 +0530

    string

--- student.java
+++ student.java
@@ -26,6 +26,7 @@ class newstudent extends student1{
     }
 }
 public class student {
+    @SuppressWarnings("resource")
     public static void main(String args[]){
         Scanner i = new Scanner(System.in);
         System.out.println("roll: ");
